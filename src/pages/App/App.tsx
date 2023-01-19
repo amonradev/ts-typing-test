c2976d764d9afef93caad47095e5d7309dec89f2
amonradev <amonracardoso@gmail.com>
2023-01-18 21:58:11 -0300

    refactor: Changing app structure

---
+++ src/pages/App/App.tsx
@@ -0,0 +1,11 @@
+import './App.styles.css';
+
+function App() {
+    return (
+        <div className="container">
+            <div className="containerContent"></div>
+        </div>
+    );
+}
+
+export default App;
